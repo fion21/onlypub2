416b2dc978a9bf306dbc978f171e6b9b7de17dfa
Fion L <hwindepth@yahoo.com>
2022-02-09 23:03:49 +0000

    Part 1

---
+++ src/views/About.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p class="text-red-900 transform -rotate-45"> test if tailwind is working or not</p>
+ <div class="text-blue-900 bg-white p-5">
+            dummy text?
+          </div>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <main>
+   <p class="text-red-900 transform -rotate-45"> test if tailwind is working or not</p>
+ <div class="text-blue-900 bg-white p-5">
+            dummy text?
+          </div>
+  </main>
+</template>
+
+
